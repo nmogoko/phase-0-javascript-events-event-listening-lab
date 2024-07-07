10569b31bb7915959f7b7cd7b34793365d84f3be
nmogoko <+>
2024-07-07 13:20:34 +0100

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// function addingEventListener() {
+//  alert("I was clicked")}
+
+// The following returns a false when test is run instead of true.
+//  const input = document.getElementById("button").addEventListener("click", addingEventListener, true)
+
+//  button.onclick = addingEventListener;
+// button.addEventListener (alert, addingEventListener);
+
+
+const input = document.getElementById('button');
 function addingEventListener() {
-}
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+      })
+};
+
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
